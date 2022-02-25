11c19dc55ae269a0ef280a200a8e6ba45f937dff
aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-02-25 14:57:31 +0530

    Fixes writing a new post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,8 +3,9 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 
 //CREATE POST
-router.post("/", async (req, res) => {
+router.post("/new/:name", async (req, res) => {
   const newPost = new Post(req.body);
+  newPost.username = req.params.name;
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
